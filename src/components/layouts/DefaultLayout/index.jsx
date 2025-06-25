fdc7b7c50abb3d66250b9d9553e791193815fad0
unknown <28.taufiqurrahman@gmail.com>
2025-06-25 15:34:18 +0700

    menambahkan location.replace dengan useNavigate di DefaultLayout

--- src/components/layouts/DefaultLayout/index.jsx
+++ src/components/layouts/DefaultLayout/index.jsx
@@ -2,6 +2,8 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth } from '../../../utils/firebase'
 import { useAtom } from 'jotai'
 import { emailStorageAtom, tokenStorageAtom } from '../../../jotai/atoms'
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 const DefaultLayout = ({ children }) => {
     const [user, loading, error] = useAuthState(auth)
@@ -9,11 +11,17 @@ const DefaultLayout = ({ children }) => {
     const [emailStorage] = useAtom(emailStorageAtom)
     const [tokenStorage] = useAtom(tokenStorageAtom)
 
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user && emailStorage && tokenStorage) {
+            navigate("/browse")
+        }
+    }, [user, emailStorage, tokenStorage, navigate])
+
     if (loading) return <p>Loading...</p>
 
     if (error) return <p>Error!!!</p>
-
-    if (user && emailStorage && tokenStorage) return location.replace('/browse')
 
     return (
         <div>
